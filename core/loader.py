bf68269a3593d98be343ab6259f85face4e87d36
milossimic <milossimicsimo@gmail.com>
2016-02-21 21:47:13 +0100

    added doc to loader class

--- core/loader.py
+++ core/loader.py
@@ -4,6 +4,14 @@ from os.path import isfile, join
 from utils import clear, eliminate, PY_EXT
 
 class PluginLoader(object):
+	'''
+		Class that do work on loading plugins from specific folder.
+
+		Args:
+			plugins_dir (string): path to specific plugin folder
+			auto_load_plugins (Optional [bool]): doese the plugin folder load plugins authomatic
+	'''
+
 	def __init__(self, plugins_dir, auto_load_plugins=False):
 		self.path = plugins_dir
 
@@ -11,13 +19,22 @@ class PluginLoader(object):
 			self.load_plugins()
 
 	def files_generator(self, path):
+		'''
+			Generator function, that will generate file by file
+
+			Args:
+				path (string): path from where to read
+		'''
+
 		for file in listdir(path):
 			if isfile(join(path, file)) and eliminate(file):
 				yield file
 
 	def load_plugins(self):
 		'''
-			Raise:
+			Function that load plugins from specific path. Generator function load one at the time.
+
+			Raises:
 				ImportError : if module or plugins folder don't exists
 		'''
 
@@ -26,18 +43,39 @@ class PluginLoader(object):
 			if PY_EXT in ext:
 				yield self.get_from_path(file, self.path)
 
-	def load_plugin(self, path, folder=None):
+	def load_plugin(self, file, folder=None):
+		'''
+			Load single plugin from some specificic folder.
+
+			Args:
+				file (string): filename of single plugin to load
+				file (string): folder name where to look for plugins.
+
+			Returns:
+				module (module): return module that contains class that inherate Plugin class
+
+		'''
+
 		if not folder:
 			folder = self.path
 
-		return self.get_from_path(path, folder)
+		return self.get_from_path(file, folder)
 
 	def reload_plugin(self, file):
 		return self.get_from_path(file, self.path)
 
 	def get_from_path(self, file, folder):
 		'''
-			Raise:
+			Function that loads specific file from specific folder.
+
+			Args:
+				file (string): name of file to load
+				folder (string): name of folder where to look for plugin
+
+			Returns:
+				module (python module): loaded python module
+
+			Raises:
 				ImportError : if module can't be find
 		'''
 
@@ -48,5 +86,17 @@ class PluginLoader(object):
 		return module
 		
 	def create_module(self, name, f, filename, description):
+		'''
+			Load single module.
+
+			name (string): name of module
+			f (file): file of single module
+			filename (string): filename of module
+			description (string): description of module
+
+			Returns:
+				loaded python module
+		'''
+
 		return load_module(name, f, filename, description)
 		
